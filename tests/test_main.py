d7bec9825dead7cba4998b07d32d9bc9cb7f2cd2
vsvsilva <vitor_ovitor@outlook.com>
2025-03-25 15:35:28 -0300

    update mock response

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,10 +19,10 @@ def test_get_valid_currency(monkeypatch):
 
 def test_get_invalid_currency(monkeypatch):
     def mock_get_currency(from_currency):
-        return {"error": "Invalid currency"}
+        return {"error": "Currency 'INVALID' not found"}  # Updated mock response
 
     # Substitui a função real "get_currency" pelo mock
     monkeypatch.setattr("app.currency.get_currency", mock_get_currency)
     response = client.get("/get/?from_currency=INVALID")
-    assert response.status_code == 400 
-    assert response.json() == {"detail": "Invalid currency"}
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Currency 'INVALID' not found"}  # Updated assertion
